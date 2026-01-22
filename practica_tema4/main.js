0c5427aea63f7b8cfef0688a6cceda786bbdaaab
Mochii1312 <esoo0006@alu.medac.es>
2026-01-22 13:07:35 +0100

    ablero terminado

--- practica_tema4/main.js
+++ practica_tema4/main.js
@@ -1,17 +1,17 @@
-const tab = document.getElementById("tab");
-const resetear = document.getElementById("resetear");
+let datosTablero = { columnas: []};
 
 document.addEventListener("DOMContentLoaded", () =>{
     const guardarDatos =localStorage.getItem("tablero");
 
     if(guardarDatos){
-        initTablero(JSON.parse(guardarDatos));
+        datosTablero = JSON.parse(guardarDatos);
+        gestionTablero();
     }else{
         mostrarFormulario();
     }
 });
 
-function guardar(datos){
+function guardar(){
     localStorage.setItem("tablero", JSON.stringify(datos));
 }
 
@@ -19,5 +19,67 @@ function mostrarFormulario(){
     const contenedor =document.getElementById("tab");
     contenedor.textContent = "";
 
+    const divConfiguracion = document.createElement("div");
+    divConfiguracion.className = "configContenedor";
+
+    const h2 = document.createElement("h2");
+    h2.textContent = "Configuracion del tablero";
+
+    const p = document.createElement("p")
+    p.textContent = "Introduce el numero de columnas que quieres";
+
+    const inputNumero = document.createElement("input");
+    inputNumero.type = "number";
+    inputNumero.min = 1;
+    inputNumero.max = 6;
+    inputNumero.value = 3;
+
+    const crearColumnas = document.createElement("div");
+
+    const generarColumnas = () =>{
+        crearColumnas.textContent = " ";
+        for(let i = 0; i < inputNumero.value; i++){
+            const columns = document.createElement("div");
+            columns.className = "configColumnas";
+
+            const nombreColumnas = document.createElement("input");
+            nombreColumnas.className = "nombreColumna";
+            nombreColumnas.placeholder = `Nombre columna ${i + 1}`;
+
+            const limiteColumnas = document.createElement("input");
+            limiteColumnas.type = "number";
+            limiteColumnas.className = "limiteColumns"
+            limiteColumnas.placeholder = "Limite de tareas";
+            limiteColumnas.value = 5;
+
+            columns.append(nombreColumnas, limiteColumnas);
+            crearColumnas.appendChild(columns);
+        }
+    };
+    inputNumero.addEventListener("change" , generarColumnas);
+    generarColumnas();
+
+    const botonEnviar = document.createElement("boton");
+    botonEnviar.textContent = "Crear tablero";
+    botonEnviar.onclick = () =>{
+        const nombres = document.querySelectorAll(".nombreColumna");
+        const limite = document.querySelectorAll(".limeteColumns");
+
+        datosTablero.columnas = Array.from(nombres).map((inp, i) =>({
+            id: 1,
+            titulo: inp.value || `Columna ${i + 1}`,
+            limit: parseInt(limite[i].value) || 5,
+            tareas: []
+            
+        }));
+        guardar();
+        mostrarFormulario();
+    };
+
+    columns.append(h2, p, inputNumero, crearColumnas, botonEnviar);
+    contenedor.appendChild(columns);
+}
+
+function gestionTablero(){
     
 }
